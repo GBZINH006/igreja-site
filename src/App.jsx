09e0762aa5c0b5487ddce63eec2910383d3027ed
GBZINH006 <gabrieldossantosdutra06@gmail.com>
2026-01-14 16:43:43 -0300

    app, cadastro, home

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,43 @@
 import React from 'react';
-// 1. Importação do Provedor de Temas
+// 1. Provedor de Temas do PrimeReact
 import { PrimeReactProvider } from 'primereact/api';
 
-// 2. Importação dos estilos essenciais do PrimeReact
-import "primereact/resources/themes/lara-light-blue/theme.css";  // Tema moderno
-import "primereact/resources/primereact.min.css";                // Core CSS
-import "primeicons/primeicons.css";                              // Ícones
-import "primeflex/primeflex.css";                                // Layout (Grid/Flex)
+// 2. Estilos Oficiais (Importante para o visual de 2026)
+import "primereact/resources/themes/lara-light-blue/theme.css"; 
+import "primereact/resources/primereact.min.css"; 
+import "primeicons/primeicons.css"; 
+import "primeflex/primeflex.css"; 
 
-// 3. Importação do Sistema de Rotas (Navegação)
+// 3. Sistema de Rotas
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// 4. Importação das suas páginas que criamos
+// 4. Importação das suas Páginas
+import { Home } from './pages/Public/Home';
 import { Cadastro } from './pages/Public/Cadastro';
 import { Secretaria } from './pages/Admin/Secretaria';
+import { LoginAdmin } from './pages/Login/LoginAdmin';
+import { LojaEBD } from './pages/Membro/LojaEBD'
 
 function App() {
   return (
-    // O PrimeReactProvider deve envolver toda a aplicação para os componentes funcionarem
     <PrimeReactProvider value={{ ripple: true }}>
       <BrowserRouter>
         <Routes>
-          {/* Rota onde o MEMBRO se cadastra */}
-          <Route path="/" element={<Cadastro />} />
+          {/* Página Principal estilo AD Blumenau */}
+          <Route path="/" element={<Home />} />
+
+          {/* Página da Ficha Cadastral Completa */}
           <Route path="/cadastrar" element={<Cadastro />} />
 
-          {/* Rota onde a MÍDIA, SECRETARIA e PASTOR veem os nomes em tempo real */}
-          {/* Futuramente, você pode colocar uma senha aqui para proteger */}
-         <Route path="/secretaria" element={<Secretaria />} />
-          
-          {/* Página de erro 404 caso digitem o link errado */}
-          <Route path="*" element={<div className="p-4 text-center"><h1>Página não encontrada!</h1></div>} />
+          {/* Página de Login do Pastor/Mídia */}
+          <Route path="/admin-login" element={<LoginAdmin />} />
+
+          {/* Página de Aprovação e Download de PDF */}
+          <Route path="/secretaria" element={<Secretaria />} />
+
+
+          {/* Rota de segurança caso digitem link errado */}
+          <Route path="LojaEBD" element={<LojaEBD />} />
         </Routes>
       </BrowserRouter>
     </PrimeReactProvider>
